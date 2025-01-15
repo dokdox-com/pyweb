0493a43eb898a02f6393072a30fae533243205bf
dokdo <132822988+onepythonlove@users.noreply.github.com>
2025-01-15 13:20:15 +0900

    button is added

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ co = cohere.ClientV2(api_key)
 st.metric(label="Temperature", value="20°C", delta="약 20 도 증가됨")
 #링크
 st.markdown("[더 나은 dokdox 의 정보를 확인할려면 dokdox.com 에 방문해보세요.](https://dokdox.com)")
-
+st.link_button("dokdox 의 날씨서비스로 이동할려면 이곳을 클릭!", "https://dokdox.com")
 
 st.title("Dokdox Free AI Service")
 st.write("Dokdox is the world's most wonderful web service.")
 st.write("Thank you for using the free AI service!")
 
 # 사용자 입력 받기
-user_input = st.chat_input("대화하려면 여기에 입력하세요...")
+user_input = st.chat_input("대화하려면 여기에 입력하세요.../Type here to chat!")
 
 # 응답 히스토리 초기화
 if user_input:
@@ -35,8 +35,3 @@ if user_input:
     st.write("This AI response was generated using Cohere's language model. / Non-commercial use only.")
 
 
-
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-st.bar_chart(chart_data)
