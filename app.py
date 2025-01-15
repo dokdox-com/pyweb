ef5ad619189a9374cc9657605d9fd1c3d02f5bc4
dokdo <132822988+onepythonlove@users.noreply.github.com>
2025-01-15 13:17:44 +0900

    test

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import cohere
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 # Streamlit secrets에서 API 키 가져오기
 api_key = st.secrets["COHERE_API_KEY"]
@@ -10,11 +12,9 @@ api_key = st.secrets["COHERE_API_KEY"]
 co = cohere.ClientV2(api_key)
 
 #예시
-st.metric(label="Temperature", value="20 °C", delta="약 20 도 증가됨")
+st.metric(label="Temperature", value="20°C", delta="약 20 도 증가됨")
 #링크
 st.markdown("[더 나은 dokdox 의 정보를 확인할려면 dokdox.com 에 방문해보세요.](https://dokdox.com)")
-
-
 
 
 st.title("Dokdox Free AI Service")
@@ -33,3 +33,10 @@ if user_input:
     )
     st.chat_message("AI").write(response.message.content[0].text)
     st.write("This AI response was generated using Cohere's language model. / Non-commercial use only.")
+
+
+
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
